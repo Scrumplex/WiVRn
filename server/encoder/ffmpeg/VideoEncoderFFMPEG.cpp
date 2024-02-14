6f6aa1f4ea7069c9e2d39463b43f045840c4d707
Guillaume Meunier <guillaume.meunier@centraliens.net>
2024-02-14 21:40:26 +0100

    Add WIVRN_WERROR build option

--- server/encoder/ffmpeg/VideoEncoderFFMPEG.cpp
+++ server/encoder/ffmpeg/VideoEncoderFFMPEG.cpp
@@ -31,55 +31,6 @@ extern "C"
 
 namespace
 {
-
-bool should_keep_nal_h264(const uint8_t * header_start)
-{
-	uint8_t nal_type = (header_start[2] == 0 ? header_start[4] : header_start[3]) & 0x1F;
-	switch (nal_type)
-	{
-		case 6: // supplemental enhancement information
-		case 9: // access unit delimiter
-			return false;
-		default:
-			return true;
-	}
-}
-
-bool should_keep_nal_h265(const uint8_t * header_start)
-{
-	uint8_t nal_type = ((header_start[2] == 0 ? header_start[4] : header_start[3]) >> 1) & 0x3F;
-	switch (nal_type)
-	{
-		case 35: // access unit delimiter
-		case 39: // supplemental enhancement information
-			return false;
-		default:
-			return true;
-	}
-}
-
-void filter_NAL(const uint8_t * input, size_t input_size, std::vector<uint8_t> & out, VideoEncoderFFMPEG::Codec codec)
-{
-	if (input_size < 4)
-		return;
-	std::array<uint8_t, 3> header = {{0, 0, 1}};
-	auto end = input + input_size;
-	auto header_start = input;
-	while (header_start != end)
-	{
-		auto next_header = std::search(header_start + 3, end, header.begin(), header.end());
-		if (next_header != end and next_header[-1] == 0)
-		{
-			next_header--;
-		}
-		if (codec == VideoEncoderFFMPEG::Codec::h264 and should_keep_nal_h264(header_start))
-			out.insert(out.end(), header_start, next_header);
-		if (codec == VideoEncoderFFMPEG::Codec::h265 and should_keep_nal_h265(header_start))
-			out.insert(out.end(), header_start, next_header);
-		header_start = next_header;
-	}
-}
-
 bool set_log_level()
 {
 	const char * level = getenv("FFMPEG_LOG_LEVEL");
